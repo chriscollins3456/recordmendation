469e972310244732388f17d2eb6b3d7d6adc9aed
Chris Collins <chriscollins3456@gmail.com>
2017-11-22 16:14:35 -0500

    pushing changes to recordmendation. Problems with our get_weight function and how we are traversing the graph.

--- recommendation.py
+++ recommendation.py
@@ -1,44 +1,73 @@
 class Node:
-    def __init__(self, id):
+    def __init__(self, id, name):
         self.id = id
+        self.children = []
+        self.name = name
     def type(self):
         return 'node'
+    def add_child(self, node, weight):
+        self.children.append((node, weight))
+    def get_weight(self, node, visited = None, weight = None):
+        if self == node:
+            return weight
+        if visited is None:
+            visited = []
+        if weight is None:
+            weight = 0.0
+        if self.children == []:
+            return False
+        weights = []
+        for (child, dist) in self.children:
+            if child not in visited:
+                visited.append(child)
+                w = child.get_weight(node, visited, weight + dist)
+                if w != None:
+                    weights.append(w)
+        if weights == []:
+            return(10000)
+        else:
+            for visit in visited:
+                print(visit.name)
+            print(weights)
+            return(min(weights))
+    def rank_songs(self, songs):
+        song_weights = [(song, self.get_weight(song)) for song in songs]
+        ranked = []
+        while song_weights:
+            ranked.append(min(song_weights, key = lambda t: t[1]))
+            song_weights.remove(min(song_weights, key = lambda t: t[1]))
+        return(ranked)
 
 class User(Node):
-    def __init__(self, id):
-        Node.__init__(self, id)
     def type(self):
         return 'user'
 
 class Artist(Node):
-    def __init__(self, id, name):
-        Node.__init__(self, id)
-        self.name = name
     def type(self):
         return 'artist'
 
 class Song(Node):
-    def __init__(self, id, title):
-        Node.__init__(self, id)
-        self.title = title
     def type(self):
         return 'song'
+
 
 def subtractList(listA, listB):
     return [item for item in listA if item not in listB]
 
-def recommend(graph, user):
+
+def recommend(user):
     queue = [user]
     visit = []
     songs = []
+    likes = user.children
+    likes = [x for (x,y) in likes]
     while queue:
         vertex = queue.pop(0)
         if vertex not in visit:
             visit.append(vertex)
-            lst = graph[vertex]
-            lst = [x for (x,y) in lst]
-            queue.extend(subtractList(lst,visit))
-            if vertex.type() == 'song':
+            connections = vertex.children
+            connections = [x for (x,y) in connections]
+            queue.extend(subtractList(connections,visit))
+            if vertex.type() == 'song' and vertex not in likes:
                 songs.append(vertex)
-    print(songs)
-    return songs
+    return user.rank_songs(songs)
--- recommendation_test.py
+++ recommendation_test.py
@@ -1,7 +1,7 @@
-from recommendation import User, Artist, Song, recommend
+from recommendation import Node, User, Artist, Song, recommend
 
-u1 = User(1)
-u2 = User(2)
+u1 = User(1, 'user1')
+u2 = User(2, 'user2')
 a1 = Artist(1, 'artist1')
 a2 = Artist(2, 'artist2')
 s1 = Song(1, 'song1')
@@ -22,11 +22,45 @@ graph = {
     s5 : [(a2, 0.05), (u2, 0.2)]
 }
 
+g = Node(0, "graph")
+g.add_child(u1, 0)
+u1.add_child(s1, 0.2)
+u1.add_child(a1, 0.4)
+u1.add_child(u2, 0.9)
+u2.add_child(s5, 0.2)
+u2.add_child(a2, 0.4)
+a1.add_child(s1, 0.05)
+a1.add_child(s2, 0.05)
+a1.add_child(s3, 0.05)
+a1.add_child(u1, 0.4)
+a2.add_child(s4, 0.05)
+a2.add_child(s5, 0.05)
+a2.add_child(u2, 0.4)
+s1.add_child(a1, 0.05)
+s1.add_child(u1, 0.2)
+s2.add_child(a1, 0.05)
+s3.add_child(a1, 0.05)
+s4.add_child(a2, 0.05)
+s5.add_child(a2, 0.05)
+s5.add_child(u2, 0.2)
+
+print(u1.get_weight(u2))
+
+#for song in u1.rank_songs([s2, s3, s4, s5]):
+    #print(song[0].name)
+
+#for song in recommend(u1):
+    #print(song[0].name, song[1])
+
 #assert that everything in the list returned is a Song
-for song in recommend(graph, u1):
-    #assert song.type == 'song'
-    print(song.title)
+#for song in recommend(u1):
+    #assert song[0].type() == 'song'
+#    print(song[0].name, song[1])
 
 #assert that it is in the order of the scores
-#assert recommend(graph, u1) == [s2, s5, s4]
+#assert recommend(u1) == [(s2, 0.3), (s3, 0.3), (s5, 1.75), (s4, 1.85)]
 #assert recommend(graph, u2) == [s4]
+
+#assert that weight is greater than 0
+#assert recommend(u1)[0][1] > 0
+#assert recommend(u2)[0][1] > 0
